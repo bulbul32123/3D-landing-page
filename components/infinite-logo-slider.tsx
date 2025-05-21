eb9dba8f39bc9b121350edd644dbc9a87654bee5
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2025-05-21 16:59:01 +0600

    Update infinite-logo-slider.tsx

--- components/infinite-logo-slider.tsx
+++ components/infinite-logo-slider.tsx
@@ -9,16 +9,11 @@ const logos = [
 ]
 
 export default function InfiniteLogoSlider() {
-  // Create a sequence of logos that is definitely wide enough (4 sets)
   const singleSequence = [...logos, ...logos, ...logos, ...logos]
-
-  // We need two copies of this sequence to loop seamlessly by moving -50%
-  // This creates a very long strip: [Seq 1][Seq 2]
   const sliderContent = [...singleSequence, ...singleSequence]
 
   return (
     <div className="w-full overflow-hidden py-10 relative mask-gradient bg-transparent">
-      {/* Inject CSS keyframes locally to ensure it works without global dependencies */}
       <style jsx>{`
         @keyframes infinite-slide {
           0% {
@@ -43,7 +38,6 @@ export default function InfiniteLogoSlider() {
             key={index}
             className="relative h-[35px] w-[150px] flex items-center justify-center flex-shrink-0 mx-8 opacity-100 hover:grayscale hover:opacity-70 transition-all duration-300"
           >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src={logo.src || "/placeholder.svg"}
               alt={logo.name}
